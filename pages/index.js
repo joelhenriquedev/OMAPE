01b29c9046fe75610e62e271449f49a6760ba419
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2019-06-06 16:10:45 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+
+            <p>Lorem ipsum dolor sit amet</p>
+        </div>
+    )
+}
+
+export default Index
